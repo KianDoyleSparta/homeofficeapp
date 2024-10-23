5000bac8da16e9911a48d33510c5839254cbb4f8
jamestcole <jamestcole@yahoo.co.uk>
2024-10-23 11:55:36 +0100

    newbranch

--- src/main/java/com/sparta/homeoffice/controllers/WebController.java
+++ src/main/java/com/sparta/homeoffice/controllers/WebController.java
@@ -1,7 +1,5 @@
 package com.sparta.homeoffice.controllers;
 
-import com.sparta.homeoffice.entities.Ticket;
-import com.sparta.homeoffice.services.TicketService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,19 +7,21 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.sparta.homeoffice.entities.Ticket;
+
 @Controller
 @RequestMapping("/tickets")
 public class WebController {
 
     @GetMapping
     public String getHome(Model model) {
-        model.addAttribute("tickets", TicketService.getTickets());
+        // model.addAttribute("tickets", TicketService.getTickets());
         return "home";
     }
 
     @PostMapping
     public String postHome(@ModelAttribute Ticket ticket) {
-        TicketService.createTicket(ticket);
+        // TicketService.createTicket(ticket);
         return "redirect:/tickets";
     }
 }
