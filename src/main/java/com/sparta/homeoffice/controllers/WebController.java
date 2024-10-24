857ecdd0f92814aec01efb5821e94d86e54f3c8a
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-10-24 14:57:12 +0100

    feat: add get and post methods to controller
    - get all view added
    - post method (create ticket) added

--- src/main/java/com/sparta/homeoffice/controllers/WebController.java
+++ src/main/java/com/sparta/homeoffice/controllers/WebController.java
@@ -1,5 +1,8 @@
 package com.sparta.homeoffice.controllers;
 
+import com.sparta.homeoffice.dto.TicketDTO;
+import com.sparta.homeoffice.services.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,14 +18,34 @@ import java.util.List;
 @RequestMapping("/tickets")
 public class WebController {
 
+    private final TicketService ticketService;
+
+    @Autowired
+    public WebController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
+
     @GetMapping
     public String getHome(Model model) {
-//        model.addAttribute("tickets", TicketService.getTickets());
+        List<Ticket> tickets = ticketService.getAllTickets();
+        model.addAttribute("tickets", tickets);
         return "home";
     }
 
-    @PostMapping
-    public String postHome(@ModelAttribute Ticket ticket) {
+    @PostMapping()
+    public String postTicket(@ModelAttribute TicketDTO ticketDTO, Model model) {
+        int ticketId = ticketService.createNewTicket(
+                ticketDTO.getCategory(),
+                ticketDTO.getPriority(),
+                ticketDTO.getState(),
+                ticketDTO.getAssigned(),
+                ticketDTO.getShortDesc(),
+                ticketDTO.getDesc(),
+                ticketDTO.getType()
+        );
+        Ticket ticket = ticketService.getTicketById(ticketId);
+        model.addAttribute("ticket", ticket);
         return "redirect:/tickets";
     }
 }
